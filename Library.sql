26d115e0737989c3b9a2d9c9f8b9740c2646fb5c
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2022-05-17 12:01:10 +0530

    Update Library.sql

--- Library.sql
+++ Library.sql
@@ -1,168 +1,49 @@
 
+-- Login for customer
+DECLARE 
+    PROCEDURE loginCustomer_library(user IN VARCHAR2, pass IN VARCHAR2) 
+    IS 
+      passAux customer.password%TYPE; 
+      incorrect_password EXCEPTION; 
+    BEGIN 
+      SELECT password INTO passAux 
+      FROM customer 
+      WHERE username LIKE user; 
+       
+      IF passAux LIKE pass THEN 
+        DBMS_OUTPUT.PUT_LINE('User ' || user || ' loging succesfull'); 
+      ELSE 
+        RAISE incorrect_password; 
+      END IF; 
+       
+      EXCEPTION 
+      WHEN no_data_found OR incorrect_password THEN  
+           DBMS_OUTPUT.PUT_LINE('Incorrect username or password'); 
+                                        
+    END; 
+BEGIN 
+  loginCustomer_library('al1','alfred123'); 
+END;
 
-DROP TABLE Customer;
-DROP TABLE Employee;
-DROP TABLE Branch;
-DROP TABLE Rent;
-DROP TABLE Card;
-DROP TABLE Book;
-DROP TABLE Location;
-
-CREATE TABLE Card(
-  cardID NUMBER,
-  status VARCHAR2(1) CHECK ((status = 'A') OR (status = 'B')),
-  fines NUMBER,
-  CONSTRAINT Card_PK PRIMARY KEY (cardID));
-
-CREATE TABLE Customer(
-  customerID NUMBER,
-  name VARCHAR2(40),
-  customerAddress VARCHAR2(50),
-  phone NUMBER(9),
-  password VARCHAR2(20),
-  userName VARCHAR2(10),
-  dateSignUp DATE,
-  cardNumber NUMBER,
-  CONSTRAINT Customer_PK PRIMARY KEY (customerID));
-
-CREATE TABLE Employee(
-  employeeID NUMBER,
-  name VARCHAR2(40),
-  employeeAddress VARCHAR2(50),
-  phone NUMBER(9),
-  password VARCHAR2(20),
-  userName VARCHAR2(10),
-  paycheck NUMBER (8, 2),
-  branchName VARCHAR2(40),
-  cardNumber NUMBER,
-  CONSTRAINT Employee_PK PRIMARY KEY (employeeID));
-
-CREATE TABLE Branch(
-  name VARCHAR2(40),
-  address VARCHAR2(50),
-  phone NUMBER(9),
-  CONSTRAINT Branch_PK PRIMARY KEY (name));
-
-CREATE TABLE Location(
-  address VARCHAR2(50),
-  CONSTRAINT Location_PK PRIMARY KEY (address));
-
-CREATE TABLE Rent(
-  cardID NUMBER,
-  itemID VARCHAR2(6),
-  apporpriationDate DATE,
-  returnDate DATE,
-  CONSTRAINT Rent_PK PRIMARY KEY (itemID));
-
-CREATE TABLE Book(
-  ISBN VARCHAR2(4),
-  bookID VARCHAR2(6),
-  state VARCHAR2(10),
-  avalability VARCHAR2(1) CHECK ((avalability = 'A') OR (avalability = 'O')),
-  debyCost NUMBER(10,2),
-  lostCost NUMBER(10,2),
-  address VARCHAR2(50),
-  CONSTRAINT Book_PK PRIMARY KEY (bookID));
-
-
-ALTER TABLE Customer
-ADD CONSTRAINT Customer_FK
-FOREIGN KEY (cardNumber)
-REFERENCES Card(cardID);
-
-ALTER TABLE Employee
-ADD CONSTRAINT Employee_FK_Card
-FOREIGN KEY (cardNumber)
-REFERENCES Card(cardID);
-
-ALTER TABLE Employee
-ADD CONSTRAINT Employee_FK_Branch
-FOREIGN KEY (branchName)
-REFERENCES Branch(name);
-
-ALTER TABLE Branch
-ADD CONSTRAINT Branch_FK
-FOREIGN KEY (address)
-REFERENCES Location(address);
-
-ALTER TABLE Book
-ADD CONSTRAINT Book_FK
-FOREIGN KEY (address)
-REFERENCES Location(address);
-
-
-ALTER TABLE Rent
-ADD CONSTRAINT Rent_FK_Card
-FOREIGN KEY (cardID)
-REFERENCES Card(cardID);
-
-ALTER TABLE Rent
-ADD CONSTRAINT Rent_FK_Book
-FOREIGN KEY (itemID)
-REFERENCES Book(bookID);
-
-
-INSERT INTO Card VALUES (101,'A',0);
-INSERT INTO Card VALUES (102,'A',0);
-INSERT INTO Card VALUES (103,'A',0);
-INSERT INTO Card VALUES (104,'A',0);
-INSERT INTO Card VALUES (105,'A',0);
-INSERT INTO Card VALUES (106,'A',0);
-INSERT INTO Card VALUES (107,'B',50);
-INSERT INTO Card VALUES (108,'B',10);
-INSERT INTO Card VALUES (109,'B',25.5);
-INSERT INTO Card VALUES (110,'B',15.25);
-INSERT INTO Card VALUES (151,'A',0);
-INSERT INTO Card VALUES (152,'A',0);
-INSERT INTO Card VALUES (153,'A',0);
-INSERT INTO Card VALUES (154,'A',0);
-INSERT INTO Card VALUES (155,'A',0);
-
-INSERT INTO Location VALUES ('ARCHEOLOGY ROAD');
-INSERT INTO Location VALUES ('CHEMISTRY ROAD');
-INSERT INTO Location VALUES ('COMPUTING ROAD');
-INSERT INTO Location VALUES ('PHYSICS ROAD');
-
-INSERT INTO Branch VALUES ('ARCHEOLOGY', 'ARCHEOLOGY ROAD', 645645645);
-INSERT INTO Branch VALUES ('CHEMISTRY', 'CHEMISTRY ROAD', 622622622);
-INSERT INTO Branch VALUES ('COMPUTING', 'COMPUTING ROAD', 644644644);
-INSERT INTO Branch VALUES ('PHYSICS', 'PHYSICS ROAD', 666666666);
-
-INSERT INTO Customer VALUES (1, 'ALFRED', 'BACON STREET', 623623623, 'alfred123', 'al1', '12-May-2018', 101);
-INSERT INTO Customer VALUES (2, 'JAMES', 'DOWNTOWN ABBEY', 659659659, 'james123', 'ja2', '10-May-2018', 102);
-INSERT INTO Customer VALUES (3, 'GEORGE', 'DETROIT CITY', 654654654, 'george123', 'ge3', '21-June-2017', 103);
-INSERT INTO Customer VALUES (4, 'TOM', 'WASHINGTON DC.', 658658658, 'tom123', 'tom4', '05-Dec-2016', 104);
-INSERT INTO Customer VALUES (5, 'PETER', 'CASTERLY ROCK', 652652652, 'peter123', 'pe5', '09-Aug-2016', 105);
-INSERT INTO Customer VALUES (6, 'JENNY', 'TERRAKOTA', 651651651, 'jenny123', 'je6', '30-April-2017', 106);
-INSERT INTO Customer VALUES (7, 'ROSE', 'SWEET HOME ALABAMA', 657657657, 'rose123', 'ro7', '28-July-2018', 107);
-INSERT INTO Customer VALUES (8, 'MONICA', 'FAKE STREET 123', 639639639, 'monica123', 'mo8', '15-Jan-2016', 108);
-INSERT INTO Customer VALUES (9, 'PHOEBE', 'CENTRAL PERK', 678678678, 'phoebe123', 'pho9', '25-MAr-2016', 109);
-INSERT INTO Customer VALUES (10, 'RACHEL', 'WHEREVER', 687687687, 'rachel123', 'ra10', '01-September-2017', 110);
-
-INSERT INTO Employee VALUES (211, 'ROSS', 'HIS HOUSE', 671671671, 'ross123', 'ro11', 1200, 'ARCHEOLOGY', 151);
-INSERT INTO Employee VALUES (212, 'CHANDLER', 'OUR HEARTHS', 688688688, 'chandler123', 'chand12', 1150.50, 'ARCHEOLOGY', 152);
-INSERT INTO Employee VALUES (213, 'JOEY', 'LITTLE ITAYLY', 628628628, 'joey123', 'jo13', 975.75, 'ARCHEOLOGY', 153);
-INSERT INTO Employee VALUES (214, 'VICTOR', 'SANTA FE', 654321987, 'victor123', 'vic14', 2200, 'COMPUTING', 154);
-INSERT INTO Employee VALUES (215, 'JAIRO', 'ARMILLA', 698754321, 'jairo123', 'ja15', 2200.50, 'CHEMISTRY', 155);
-
-INSERT INTO Book VALUES ('A123', 'B1A123', 'GOOD', 'A', 5, 20, 'ARCHEOLOGY ROAD');
-INSERT INTO Book VALUES ('A123', 'B2A123', 'NEW', 'O', 6, 30, 'ARCHEOLOGY ROAD');
-INSERT INTO Book VALUES ('B234', 'B1B234', 'NEW', 'A', 2, 15, 'CHEMISTRY ROAD');
-INSERT INTO Book VALUES ('C321', 'B1C321', 'BAD', 'A', 1, 10, 'PHYSICS ROAD');
-INSERT INTO Book VALUES ('H123', 'B1H123', 'GOOD', 'A', 3, 15, 'CHEMISTRY ROAD');
-INSERT INTO Book VALUES ('Z123', 'B1Z123', 'GOOD', 'O', 4, 20, 'COMPUTING ROAD');
-INSERT INTO Book VALUES ('L321', 'B1L321', 'NEW', 'O', 4, 20, 'COMPUTING ROAD');
-INSERT INTO Book VALUES ('P321', 'B1P321', 'USED', 'A', 2, 12, 'CHEMISTRY ROAD');
-
-INSERT INTO Rent VALUES (101, 'B2A123', '10-May-2018', '20-May-2018');
-INSERT INTO Rent VALUES (102, 'B1Z123', '10-May-2018', '25-May-2018');
-INSERT INTO Rent VALUES (154, 'B1L321', '04-May-2018', '26-May-2018');
-
-
-SELECT * FROM Card;
-SELECT * FROM Customer;
-SELECT * FROM Employee;
-SELECT * FROM Branch;
-SELECT * FROM Location;
-SELECT * FROM Book;
-SELECT * FROM Rent;
+-- Login for Employee
+DECLARE 
+  PROCEDURE loginEmployee_library(user IN VARCHAR2, pass IN VARCHAR2) 
+  IS 
+    passAux employee.password%TYPE; 
+   incorrect_password EXCEPTION; 
+  BEGIN 
+    SELECT password INTO passAux 
+    FROM employee 
+    WHERE username LIKE user; 
+    IF passAux LIKE pass THEN 
+     DBMS_OUTPUT.PUT_LINE('User ' || user || ' loging succesfull'); 
+    ELSE 
+    RAISE incorrect_password; 
+    END IF; 
+    EXCEPTION 
+      WHEN no_data_found OR incorrect_password THEN  
+           DBMS_OUTPUT.PUT_LINE('Incorrect username or password'); 
+    END; 
+BEGIN 
+  loginEmployee_library('ro11','ross123'); 
+END;
